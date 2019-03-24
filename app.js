8b5bf12e6bc7be36a7deadacad738c3105ef822e
jungledj_tech <junglaq@hotmail.com>
2019-03-23 21:06:54 -0500

    Reorganizacion estructura y archivos de la App de multiplicar

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-// Tipos de requires
+const { crearArchivo } = require('./multiplicar/multiplicar');
 
-const fs = require('fs'); // Los standar de NODEJS
-//const fs = require('express')  // Los desarrollados por terceros, no son nativos NODE
-//const fs = require('./fs')  // Los desarrollados por mi
+let base = 7;
 
-let base = 6;
-let data = '';
-
-for (let i = 1; i <= 10; i++) {
-    data += `${ base } * ${ i } = ${ base * i }\n`;
-}
-
-fs.writeFile(`tabla-${ base }.txt`, data, (err) => {
-    if (err) throw err;
-    console.log(`El archivo tabla-${ base }.txt ha sido creado`);
-});
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${ archivo }`));
